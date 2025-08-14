7cc8fa4bd97b17100afe30e2105c6d9c73402bf4
itzraman31 <deepraman31104@gmail.com>
2025-08-14 09:34:17 +0530

    "14-8-25"

--- Frontend/src/components/Miscellaneous/VideoCall.js
+++ Frontend/src/components/Miscellaneous/VideoCall.js
@@ -1,4 +1,11 @@
+import { useEffect,useState } from "react";
+
 const VideoCall = () => {
+  const [isVideocalling,setisVideocalling]=useState(false);
+
+  useEffect(()=>{
+
+  },[])
   return (
     <>
       <div>this is video call div</div>

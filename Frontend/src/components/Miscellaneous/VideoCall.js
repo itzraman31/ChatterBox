b4e240920ae43ad65a88a82c69faec860484a88a
itzraman31 <deepraman31104@gmail.com>
2025-08-30 02:59:33 +0530

    "30-8-25"

--- Frontend/src/components/Miscellaneous/VideoCall.js
+++ Frontend/src/components/Miscellaneous/VideoCall.js
@@ -4,8 +4,9 @@ const VideoCall = () => {
   const [isVideocalling,setisVideocalling]=useState(false);
 
   useEffect(()=>{
+    console.log("Yes it video calling...")
+  },[isVideocalling])
 
-  },[])
   return (
     <>
       <div>this is video call div</div>
